cce0cfe3ba1d20d54ce339707d67df94ebfa3cd6
Alex Di Maria <alessandro.dimaria3@outlook.it>
2021-12-23 11:09:15 +0100

    Refactoring code

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -1,23 +1,10 @@
-import Navbar from './Navbar/Navbar';
+import MainPage from './MainPage/mainPage';
 import './App.css';
-import pizzaLogo from '../Image/pizza-logo.jpg';
 
 export default function App() {
   return (
     <div>
-      <div className='container'>
-        <div className="half-width">
-          <Navbar />
-        </div>
-
-        <div className="bg-transparent half-width flex-column">
-          <h1>
-            Pizzeria template
-          </h1>
-
-          <img src={pizzaLogo} alt="Logo pizzeria" className="logo" />
-        </div>
-      </div>
+      <MainPage />
 
       <div style={{ height: "200vh" }} />
     </div>
