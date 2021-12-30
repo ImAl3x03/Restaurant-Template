a0edefa0a5bb9164b670b5643699fe924a24851f
Alex Di Maria <alessandro.dimaria3@outlook.it>
2021-12-30 23:06:48 +0100

    Added show all reviews componnet

--- src/Component/MainPage/Review/review.tsx
+++ src/Component/MainPage/Review/review.tsx
@@ -4,15 +4,34 @@ import ReviewModel from '../../../Model/review'
 import Card from "./Card/card";
 import './review.css'
 import Button from '@mui/material/Button';
+import { Dialog, DialogContent } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-const obj = {
+const buttonStyle = {
     backgroundColor: "rgba(0, 0, 0, 0.8)"
 }
+
+const useStyles = makeStyles({
+    container: {
+        height: "auto"
+    }
+})
 
 export default function Review() {
     let initialState: ReviewModel[] = [];
     const [reviews, addReview] = useState(initialState);
     let limitReview = 6; //setting limiter to 6
+
+    const [open, setOpen] = useState(false);
+    const classes = useStyles();
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     useEffect(() => {
         async function getAPI() {
@@ -42,10 +61,28 @@ export default function Review() {
             </div>
 
             <div className="button-wrap">
-                <Button variant="contained" size="large" style={obj}>Show all review</Button>
+                <Button variant="contained" size="large" style={buttonStyle} onClick={handleClickOpen}>Show all review</Button>
 
-                <Button variant="contained" size="large" style={obj} >Add new review</Button>
+                <Button variant="contained" size="large" style={buttonStyle} >Add new review</Button>
             </div>
+
+            <Dialog open={open} onClose={handleClose} className={classes.container} fullWidth={true} maxWidth={"lg"}>
+                <DialogContent>
+                    <div className="show-reviews dialog-container">
+                        {
+                            reviews.map((ele) =>
+                                <Card
+                                    id={ele.id}
+                                    name={ele.name}
+                                    text={ele.text}
+                                    star={ele.star}
+                                    key={ele.id}
+                                />
+                            )
+                        }
+                    </div>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
