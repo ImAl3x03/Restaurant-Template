2e158c979be04d22663979c62424ff68931facbd
Alex Di Maria <alessandro.dimaria3@outlook.it>
2021-12-30 20:08:13 +0100

    Added button on the bottom side of review

--- src/Component/MainPage/Review/review.tsx
+++ src/Component/MainPage/Review/review.tsx
@@ -2,8 +2,12 @@ import { useState, useEffect } from "react";
 import { getReview } from "../../../utils/review";
 import ReviewModel from '../../../Model/review'
 import Card from "./Card/card";
-
 import './review.css'
+import Button from '@mui/material/Button';
+
+const obj = {
+    backgroundColor: "rgba(0, 0, 0, 0.8)"
+}
 
 export default function Review() {
     let initialState: ReviewModel[] = [];
@@ -20,20 +24,28 @@ export default function Review() {
     }, [])
 
     return (
-        <div id="review" className="show-reviews">
-            {
-                /*sets max of mapping to variable limitReview */
-                reviews.map((ele, index) =>
-                    index < limitReview &&
-                    (<Card
-                        id={ele.id}
-                        name={ele.name}
-                        text={ele.text}
-                        star={ele.star}
-                        key={ele.id}
-                    />)
-                )
-            }
+        <div id="review">
+            <div className="show-reviews">
+                {
+                    /*sets max of mapping to variable limitReview */
+                    reviews.map((ele, index) =>
+                        index < limitReview &&
+                        (<Card
+                            id={ele.id}
+                            name={ele.name}
+                            text={ele.text}
+                            star={ele.star}
+                            key={ele.id}
+                        />)
+                    )
+                }
+            </div>
+
+            <div className="button-wrap">
+                <Button variant="contained" size="large" style={obj}>Show all review</Button>
+
+                <Button variant="contained" size="large" style={obj} >Add new review</Button>
+            </div>
         </div>
     )
 }
